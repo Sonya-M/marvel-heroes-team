a459a340353df26f8ddc4d237af0b79ad0e02b86
Sonja Musicki <sonja.musicki@gmail.com>
2021-11-05 21:36:34 +0100

    now showing confirm dialog also for delete all

--- src/components/Team/TeamList.jsx
+++ src/components/Team/TeamList.jsx
@@ -1,45 +1,68 @@
-import TeamMemberCard from "./TeamMemberCard";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { uiActions } from "../../store/ui-slice";
 import { bookmarkedActions } from "../../store/bookmarked-slice";
+
+import TeamMemberCard from "./TeamMemberCard";
+import ModalConfirmDialog from "../UI/ModalConfirmDialog";
+
 import classes from "./TeamList.module.css";
 
 export default function TeamList(props) {
   const bookmarked = useSelector((state) => state.bookmarked.bookmarkedHeroes);
   const dispatch = useDispatch();
+  const [showConfirmDialog, setShowConfirmDialog] = useState(false);
 
   const handleClose = () => {
     dispatch(uiActions.toggleShowBookmarked());
   };
 
-  const handleRemoveAll = () => {
+  const handleConfirmRemove = () => {
     dispatch(bookmarkedActions.clearAll());
     dispatch(uiActions.toggleShowBookmarked());
   };
 
+  const handleRemoveAll = () => {
+    setShowConfirmDialog(true);
+  };
+
+  const handleCancelRemove = () => {
+    setShowConfirmDialog(false);
+  };
+
   return (
-    <section className={classes.teamList}>
-      <header className={classes.header}>
-        <h2>Your Team</h2>
-        <span className={classes.closeBtn} onClick={handleClose}>
-          x
-        </span>
-      </header>
-      {bookmarked.length > 0 ? (
-        <ul>
-          {bookmarked.map((h) => (
-            <TeamMemberCard key={h.id} hero={h} />
-          ))}
-        </ul>
-      ) : (
-        <p className={`${classes.noMembers} centered`}>Start adding members!</p>
-      )}
-      {bookmarked.length > 1 ? (
-        <p className={classes.removeAll} onClick={handleRemoveAll}>
-          Expel all!
-        </p>
+    <>
+      {showConfirmDialog ? (
+        <ModalConfirmDialog
+          content="Are you sure you want to remove all heroes from your team?"
+          onCancel={handleCancelRemove}
+          onConfirm={handleConfirmRemove}
+        />
       ) : null}
-    </section>
+      <section className={classes.teamList}>
+        <header className={classes.header}>
+          <h2>Your Team</h2>
+          <span className={classes.closeBtn} onClick={handleClose}>
+            x
+          </span>
+        </header>
+        {bookmarked.length > 0 ? (
+          <ul>
+            {bookmarked.map((h) => (
+              <TeamMemberCard key={h.id} hero={h} />
+            ))}
+          </ul>
+        ) : (
+          <p className={`${classes.noMembers} centered`}>
+            Start adding members!
+          </p>
+        )}
+        {bookmarked.length > 1 ? (
+          <p className={classes.removeAll} onClick={handleRemoveAll}>
+            Expel all!
+          </p>
+        ) : null}
+      </section>
+    </>
   );
 }
