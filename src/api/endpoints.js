23b4eda72dabf74d1af15a8c6799e91a1c605e4c
Sonya-M <sonja.musicki@gmail.com>
2021-11-09 14:38:57 +0100

    https

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -5,9 +5,9 @@ e.g.
 https://gateway.marvel.com:443/v1/public/characters?limit=20&offset=20&apikey=[KEY]
 */
 
-export const ALL_CHARACTERS = "http://gateway.marvel.com/v1/public/characters";
+export const ALL_CHARACTERS = "https://gateway.marvel.com/v1/public/characters";
 
 // e.g. https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=Hulk%20Incredible&apikey=[KEY]
 
 
-export const ALL_COMICS = "http://gateway.marvel.com/v1/public/comics"
+export const ALL_COMICS = "https://gateway.marvel.com/v1/public/comics"
